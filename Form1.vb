6e27de4bb7eba8d26307e187f8b37f1d383b32a3
tsukamoto0427 <trackandfieldakihisa@yahoo.co.jp>
2018-01-24 12:14:46 +0900

    CSV形式での出力
    CSV形式でファイルを出力しました

--- Form1.vb
+++ Form1.vb
@@ -381,7 +381,7 @@ Public Class Form1
         '
         'Form1
         '
-        Me.AutoScaleBaseSize = New System.Drawing.Size(7, 15)
+        Me.AutoScaleBaseSize = New System.Drawing.Size(5, 12)
         Me.ClientSize = New System.Drawing.Size(736, 505)
         Me.IsMdiContainer = True
         Me.Menu = Me.MainMenu1
@@ -453,4 +453,35 @@ Public Class Form1
         fm.Show()
     End Sub
 
+    Private Sub mnuToolExportCSV_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mnuToolExportCsv.Click
+        Dim sw As IO.StreamWriter    'StreamWriterオブジェクト
+        Dim i As Integer    'カウンタ
+        Dim sbuf As String  'ファイルに出力するデータ
+
+        '［名前を付けて保存］ダイアログ
+        dlgSave.InitialDirectory = Application.StartupPath & "\.."
+        dlgSave.Filter = "CSV形式（*.csv）|*.csv|XML形式(*.xml)|*.xml|すべて（*.*）|*.*"
+        If dlgSave.ShowDialog = DialogResult.Cancel Then
+            Exit Sub
+        End If
+
+        'データ読み込み
+        odaShohin.Fill(DsSample1, "T_商品")
+
+        'ファイルオープン
+        sw = New IO.StreamWriter(dlgSave.FileName,
+          False, System.Text.Encoding.GetEncoding("Shift-JIS"))
+
+        'ファイルに出力
+        For i = 0 To DsSample1.T_商品.Rows.Count - 1
+            sbuf = DsSample1.T_商品.Rows(i)("商品番号") & "," _
+         & DsSample1.T_商品.Rows(i)("商品名") & "," _
+         & DsSample1.T_商品.Rows(i)("単価") & "," _
+         & DsSample1.T_商品.Rows(i)("商品グループ")
+            sw.WriteLine(sbuf)
+        Next
+        'ファイルクローズ
+        sw.Close()
+        MessageBox.Show("保存しました", "エクスポート")
+    End Sub
 End Class
